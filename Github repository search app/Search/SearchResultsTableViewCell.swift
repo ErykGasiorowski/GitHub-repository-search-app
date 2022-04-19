92af9b6f85c0c1e32d27556f621ab0c626fb28c6
ErykGasiorowski <76919604+ErykGasiorowski@users.noreply.github.com>
2022-04-20 00:07:07 +0200

    fixed issues with constraints in tableViewCell. Next step - map starsNumber from int to string and pass data to label. Also, fix issue with ownersImage not being displayed in the tableview.

--- Github repository search app/Search/SearchResultsTableViewCell.swift
+++ Github repository search app/Search/SearchResultsTableViewCell.swift
@@ -20,7 +20,7 @@ class SearchResultsTableViewCell: UITableViewCell {
     private let ownerImage: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(systemName: "image")
-        imageView.contentMode = .scaleAspectFill
+        imageView.contentMode = .scaleAspectFit
         imageView.layer.cornerRadius = 9
         imageView.clipsToBounds = true
         
@@ -30,7 +30,7 @@ class SearchResultsTableViewCell: UITableViewCell {
     private let repoTitleLabel: UILabel = {
         let label = UILabel()
         label.text = "repo title"
-        label.font = .systemFont(ofSize: 14, weight: .bold)
+        label.font = .systemFont(ofSize: 20, weight: .bold)
         label.textColor = .black
         label.numberOfLines = 1
         return label
@@ -40,6 +40,7 @@ class SearchResultsTableViewCell: UITableViewCell {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
         imageView.image = UIImage(systemName: "star")
+        imageView.tintColor = .systemGray
         imageView.clipsToBounds = true
         
         return imageView
@@ -48,7 +49,7 @@ class SearchResultsTableViewCell: UITableViewCell {
     private let starsLabel: UILabel = {
         let label = UILabel()
         label.text = "number of stars from endpoint"
-        label.font = .systemFont(ofSize: 14, weight: .light)
+        label.font = .systemFont(ofSize: 18, weight: .light)
         label.textColor = .systemGray
         label.numberOfLines = 1
         return label
@@ -80,7 +81,7 @@ class SearchResultsTableViewCell: UITableViewCell {
             starsLabel.text = String(stars)
         }
         else {
-            starsLabel.text = "-"
+            starsLabel.text = "999"
         }
     }
 
@@ -92,33 +93,34 @@ class SearchResultsTableViewCell: UITableViewCell {
         contentView.addSubview(starsLabel)
         contentView.addSubview(button)
 
-//        ownerImage.snp.makeConstraints {
-//            $0.top.equalTo(contentView.snp.top).offset(10)
-//            $0.bottom.equalTo(contentView.snp.bottom).inset(10)
-//            $0.leading.equalTo(contentView.snp.leading).offset(10)
-//            $0.centerY.equalTo(contentView.snp.centerY)
-//        }
+        ownerImage.snp.makeConstraints {
+            $0.top.equalTo(contentView.snp.top).offset(10)
+            $0.bottom.equalTo(contentView.snp.bottom).inset(10)
+            $0.leading.equalTo(contentView.snp.leading).offset(10)
+            $0.width.equalTo(60)
+            $0.height.equalTo(60)
+            $0.centerY.equalTo(contentView.snp.centerY)
+        }
 
         repoTitleLabel.snp.makeConstraints{
             $0.top.equalTo(contentView.snp.top).offset(10)
-                       $0.bottom.equalTo(contentView.snp.bottom).inset(10)
-                       $0.leading.equalTo(contentView.snp.leading).offset(10)
-                       $0.centerY.equalTo(contentView.snp.centerY)
-//            $0.leading.equalTo(ownerImage.snp.trailing).offset(10)
-//            $0.bottom.equalTo(ownerImage.snp.centerY)
-//            $0.trailing.equalTo(contentView.snp.trailing)
+            $0.leading.equalTo(ownerImage.snp.trailing).offset(10)
+            $0.bottom.equalTo(ownerImage.snp.centerY)
+            $0.trailing.equalTo(contentView.snp.trailing)
         }
-//
-//        starImage.snp.makeConstraints{
-//            $0.leading.equalTo(ownerImage.snp.trailing).offset(10)
-//            $0.top.equalTo(ownerImage.snp.centerY)
-//        }
-//
-//        repoTitleLabel.snp.makeConstraints{
-//            $0.leading.equalTo(starImage.snp.trailing).offset(2)
-//            $0.top.equalTo(ownerImage.snp.centerY)
-//            $0.trailing.equalTo(contentView.snp.trailing)
-//        }
+
+        starImage.snp.makeConstraints{
+            $0.leading.equalTo(ownerImage.snp.trailing).offset(10)
+            $0.top.equalTo(ownerImage.snp.centerY)
+            $0.width.equalTo(20)
+        }
+
+        starsLabel.snp.makeConstraints{
+            $0.leading.equalTo(starImage.snp.trailing).offset(10)
+            $0.top.equalTo(ownerImage.snp.centerY)
+            $0.trailing.equalTo(contentView.snp.trailing)
+//            $0.bottom.equalTo(contentView.snp.bottom).inset(10)
+        }
 //
 //        button.snp.makeConstraints {
 //            $0.top.equalTo(contentView.snp.top)
