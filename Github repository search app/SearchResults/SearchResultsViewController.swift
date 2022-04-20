ab02457c76632d8bc0b387bc725d825239d90ca4
ErykGasiorowski <76919604+ErykGasiorowski@users.noreply.github.com>
2022-04-20 23:33:40 +0200

    added a few labes and images to 2nd view. todo -> finish 2nd view, preferably using 2 different views.

--- Github repository search app/SearchResults/SearchResultsViewController.swift
+++ Github repository search app/SearchResults/SearchResultsViewController.swift
@@ -9,8 +9,55 @@ import UIKit
 
 class SearchResultsViewController: UIViewController {
 
-    var repoImageView = RepoImageView()
-    var repoDetailsView = RepoDetailsView()
+//    var repoImageView = RepoImageView()
+//    var repoDetailsView = RepoDetailsView()
+    
+    private let repoImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "image")
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .red
+        imageView.clipsToBounds = true
+        
+        return imageView
+    }()
+    
+    private let repoByLabel: UILabel = {
+        let label = UILabel()
+        label.text = "repo's author name"
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        label.textColor = .white
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    private let repoAuthorNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "repo's author name"
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        label.textColor = .white
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    private let starsCountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Number of Stars ()"
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        label.textColor = .white
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    private let starImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "star")
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        
+        return imageView
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +68,8 @@ class SearchResultsViewController: UIViewController {
     
     
     func layoutView() {
-        view.addSubview(repoImageView)
-        view.addSubview(repoDetailsView)
+//        view.addSubview(repoImageView)
+//        view.addSubview(repoDetailsView)
+    
     }
 }
